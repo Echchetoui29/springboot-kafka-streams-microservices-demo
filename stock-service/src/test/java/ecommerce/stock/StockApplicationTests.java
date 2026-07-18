a50d3add237b29f3250fed0f1f45176db9acd91b
Echchetoui29 <t.echchetoui2914@uca.ac.ma>
2026-07-18 01:48:20 +0100

    gitlab ci setup - Test config service-v2

--- stock-service/src/test/java/ecommerce/stock/StockApplicationTests.java
+++ stock-service/src/test/java/ecommerce/stock/StockApplicationTests.java
@@ -2,8 +2,12 @@ package ecommerce.stock;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@EmbeddedKafka(partitions = 1)
 class StockApplicationTests {
 
     @Test
